3a4bd2290cee7d22bc1da8ddb5cca2357e17d126
Allure <samira-h@hotmail.co.uk>
2022-02-26 15:53:56 +0000

    array of questions

--- index.js
+++ index.js
@@ -2,3 +2,52 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project in a few words ?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instructions for your project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the purpose of this project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the suitble license for this project',
+        choices: ['mit', 'apache', 'no license']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Type in the contribution guidelines for your project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Insert your test instructions here',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub Username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+];
